912e62a6c2312a30226f96b7816c8bf06ca930ad
rdiazca <rdiazca96@gmail.com>
2022-02-17 10:13:24 -0500

    Métodos del ciclo de vida del componente

--- src/components/ListaProductos.js
+++ src/components/ListaProductos.js
@@ -3,15 +3,34 @@ import Producto from "./Producto";
 
 class ListaProductos extends Component {
     state = {
-        productos : [
-            { id : 1, nombre : "Camisa ReactJS", precio: 30 },
-            { id : 2, nombre : "Camisa VueJS", precio: 30 },
-            { id : 3, nombre : "Camisa Angular", precio: 30 },
-            { id : 4, nombre : "Camisa Node.js", precio: 30 }
-        ]
+        productos : []
     }
-    render() {
 
+    componentDidMount(){
+        console.log(1);
+
+        this.setState({
+            productos : [
+                { id : 1, nombre : "Camisa ReactJS", precio: 30 },
+                { id : 2, nombre : "Camisa VueJS", precio: 30 },
+                { id : 3, nombre : "Camisa Angular", precio: 30 },
+                { id : 4, nombre : "Camisa Node.js", precio: 30 }
+            ]
+        })
+
+    }
+    componentWillMount(){
+        console.log(2);
+    }
+    componentWillUpdate(){
+        console.log(3);
+    }
+    componentWillUnmount(){
+        console.log(4);
+    }
+
+    render() {
+        console.log(5);
         const productos = this.state.productos;
         
         return ( 
